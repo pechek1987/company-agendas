6422f8d4034f0de2896fd39d7b60600ad16b33b3
pechek1987 <michael@pechekfamily.com>
2026-06-13 16:32:30 -0500

    Update index.js

--- api/deleteHistory/index.js
+++ api/deleteHistory/index.js
@@ -1,12 +1,24 @@
 const { CosmosClient } = require("@azure/cosmos");
-const client = new CosmosClient({ endpoint: process.env.COSMOS_ENDPOINT, key: process.env.COSMOS_KEY });
-const DB = "agendas", CONTAINER = "history";
+
 module.exports = async function (context, req) {
-  context.res = { headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } };
+  context.res = {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  };
   try {
+    const client = new CosmosClient({
+      endpoint: process.env.COSMOS_ENDPOINT,
+      key: process.env.COSMOS_KEY
+    });
     const { agendaKey, date } = context.bindingData;
     const id = agendaKey + "_" + date;
-    await client.database(DB).container(CONTAINER).item(id, agendaKey).delete();
+    await client
+      .database("agendas")
+      .container("history")
+      .item(id, agendaKey)
+      .delete();
     context.res.body = JSON.stringify({ success: true });
   } catch (err) {
     context.res.status = 500;
