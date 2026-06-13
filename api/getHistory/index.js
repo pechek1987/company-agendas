73700d326a718b97856e01aec352ce1b94c545a9
pechek1987 <michael@pechekfamily.com>
2026-06-13 16:46:14 -0500

    Update index.js

--- api/getHistory/index.js
+++ api/getHistory/index.js
@@ -1,30 +1,18 @@
-const { CosmosClient } = require("@azure/cosmos");
+const { request } = require("../cosmos");
 
 module.exports = async function (context, req) {
-  context.res = {
-    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }
-  };
+  context.res = { headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } };
   try {
     const endpoint = process.env.COSMOS_ENDPOINT;
     const key = process.env.COSMOS_KEY;
-    if (!endpoint || !key) {
-      context.res.status = 500;
-      context.res.body = JSON.stringify({ error: "Missing env vars", hasEndpoint: !!endpoint, hasKey: !!key });
-      return;
-    }
-    const client = new CosmosClient({ endpoint, key });
     const agendaKey = context.bindingData.agendaKey;
-    const { resources } = await client
-      .database("agendas")
-      .container("history")
-      .items.query({
-        query: "SELECT * FROM c WHERE c.agendaKey = @key ORDER BY c.date DESC",
-        parameters: [{ name: "@key", value: agendaKey }]
-      })
-      .fetchAll();
-    context.res.body = JSON.stringify(resources);
+    const result = await request("POST", "dbs/agendas/colls/history/docs", {
+      query: "SELECT * FROM c WHERE c.agendaKey = @key ORDER BY c.date DESC",
+      parameters: [{ name: "@key", value: agendaKey }]
+    }, key, endpoint);
+    context.res.body = JSON.stringify(result.body.Documents || []);
   } catch (err) {
     context.res.status = 500;
-    context.res.body = JSON.stringify({ error: err.message, name: err.name, code: err.code });
+    context.res.body = JSON.stringify({ error: err.message });
   }
 };
